089f37733679736b7b0b86710baa968bd91f7fc5
Fantabulous <fan@fantabulous.tech>
2021-04-04 23:35:44 +1200

    Update build

--- docs/Build/docs.framework.js
+++ docs/Build/docs.framework.js
@@ -1295,7 +1295,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2000496;
+STATICTOP = STATIC_BASE + 2000688;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3281,7 +3281,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2000496;
+var STATIC_BUMP = 2000688;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
