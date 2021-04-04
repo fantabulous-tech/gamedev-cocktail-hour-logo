9e9c6a10af666d42c11eaca4ee58531471833c9f
Fantabulous <fan@fantabulous.tech>
2021-04-04 23:32:26 +1200

    Hide cursor when in the game window

--- Assets/Scripts/FollowCursor.cs
+++ Assets/Scripts/FollowCursor.cs
@@ -4,6 +4,10 @@ public class FollowCursor : MonoBehaviour {
     [SerializeField] private Camera m_Camera;
     [SerializeField] private Rigidbody2D m_Rigidbody2D;
 
+    private void Start() {
+        Cursor.visible = false;
+    }
+
     private void Update() {
         Vector3 positon = m_Camera.ScreenToWorldPoint(Input.mousePosition);
         positon.Scale(new Vector3(1, 1, 0));
